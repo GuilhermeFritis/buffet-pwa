915e09b765528fc1bf5ab14f2e38fea633c7106d
leoap <leoaguiarpereira@gmail.com>
2021-03-19 21:50:24 -0300

    Aula 08

--- Buffet/Controllers/HomeController.cs
+++ Buffet/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Buffet.Models;
+using Buffet.Models.Buffet;
 using Buffet.Models.Buffet.Cliente;
 using Buffet.ViewModels.Home;
+using Buffet.ViewModels.Shared;
 
 namespace Buffet.Controllers
 {
@@ -32,6 +34,11 @@ namespace Buffet.Controllers
             var viewmodel = new IndexViewModel();
             viewmodel.InformacaoQualquer = "Informação pela View Model";
             viewmodel.Titulo = "Título qualquer";
+            viewmodel.UsuarioLogado = new Usuario()
+            {
+                Nome = "Leonardo",
+                Idade = 34
+            };
             
             return View(viewmodel);
         }
@@ -61,6 +68,77 @@ namespace Buffet.Controllers
             return View(viewModel);
         }
 
+        public IActionResult StatusEvento()
+        {
+            // Acessando um service para obter a lista de
+            // Status dos Eventos
+            var listaStatusEventos = new List<StatusEvento>();
+            listaStatusEventos.Add(new StatusEvento()
+            {
+                Id = 1,
+                Descricao = "Reservado"
+            });
+            listaStatusEventos.Add(new StatusEvento()
+            {
+                Id = 2,
+                Descricao = "Confirmado"
+            });
+            listaStatusEventos.Add(new StatusEvento()
+            {
+                Id = 3,
+                Descricao = "Realizado"
+            });
+            
+            // Crio a view model
+            var viewmodel = new StatusEventoViewModel();
+            
+            // Alimento a view model com os dados dos status
+            foreach (StatusEvento statusEvento in listaStatusEventos) {
+                viewmodel.ListaDeStatus.Add(new Status()
+                {
+                    Id = statusEvento.Id,
+                    Descricao = statusEvento.Descricao,
+                });
+            }
+            
+            return View(viewmodel);
+        }
+        
+        public IActionResult StatusConvidado()
+        {
+            // Acessando um service para obter a lista de
+            // Status dos Convidados
+            var listaStatusConvidado = new List<StatusConvidado>();
+            listaStatusConvidado.Add(new StatusConvidado()
+            {
+                Id = 1,
+                Descricao = "A Confirmar"
+            });
+            listaStatusConvidado.Add(new StatusConvidado()
+            {
+                Id = 2,
+                Descricao = "Confirmado"
+            });
+       
+            // Crio a view model
+            var viewmodel = new StatusConvidadoViewModel();
+            
+            // Alimento a view model com os dados dos status
+            foreach (StatusConvidado statusConvidado in listaStatusConvidado) {
+                viewmodel.ListaDeStatus.Add(new Status()
+                {
+                    Id = statusConvidado.Id,
+                    Descricao = statusConvidado.Descricao,
+                });
+            }
+            
+            return View(viewmodel);
+        }
+        
+        
+        
+        
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
